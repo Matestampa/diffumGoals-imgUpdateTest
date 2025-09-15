634a11d5d308b9e4ed644604ed9ef0842cf3b83e
Matestampa <paredpiso5@gmail.com>
2025-09-14 22:22:13 -0300

    Error_alarmsTesting

--- nashe.js
+++ nashe.js
@@ -2,7 +2,7 @@
 const {InternalError,internalError_handler} = require("./error_handling")
 const {MongoDB_Connection_Error,MongoDB_Error,S3_Error,CleanCache_Error} = require("./update/errors.js")
     
-const {connect_MongoDB} = require("./mongodb")
+const {connect_MongoDB,disconnect_MongoDB} = require("./mongodb")
 const {S3_FUNCS} = require("./aws_services");
 const {updateMulti_Goals_2Db} = require("./update/getters_savers.js");
 
@@ -11,19 +11,20 @@ async function dale(){
     //-------------- MongoDB CONNECTION ERROR TEST ----------------
     try{
         await connect_MongoDB()
+        await disconnect_MongoDB()
     }
     catch(e){
         internalError_handler(new MongoDB_Connection_Error("Error de conexion a la base de datos",e),"Connecting to mongo",["All"]);
     }
 
-    /*-------------- Mongodb ERROR TEST ----------------
+    //-------------- Mongodb ERROR TEST ----------------
     try{
         await updateMulti_Goals_2Db([1,2,3])
     }
     catch(e){
         console.log(e)
         internalError_handler(new MongoDB_Error("nose",e),"Update goals",[1,2,3]);
-    }*/
+    }
     
     //-------------- S3 ERROR TEST ----------------
     try{
@@ -35,6 +36,14 @@ async function dale(){
         //console.log(e instanceof Error)
         //console.log(e)
         internalError_handler(new S3_Error("Error al obtener el archivo de S3",e),"Download",[1,2,3]);
+    }
+
+    //-------------- CLEAN CACHE ERROR TEST ----------------
+    try{
+        throw new Error("HOLA")
+    }
+    catch(e){
+        internalError_handler(new CleanCache_Error("Error al limpiar la cache",e),"CleanCache",[1,2,3]);
     }
 }
 
